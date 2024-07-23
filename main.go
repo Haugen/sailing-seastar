880d4dc8f68e27dec6bbddb931c12c0d146092e2
Haugen <tobiashaugen@gmail.com>
2024-07-23 11:25:59 +0200

    Use aisstream

--- main.go
+++ main.go
@@ -1,35 +1,71 @@
 package main
 
 import (
-	"context"
+	"encoding/json"
+	"fmt"
 	"log"
+	"os"
+	"time"
 
-	"github.com/chromedp/chromedp"
+	aisstream "github.com/aisstream/ais-message-models/golang/aisStream"
+	"github.com/gorilla/websocket"
+	"github.com/joho/godotenv"
 )
 
-// Example of using chromedp to navigate to vesselfinder.
-// Not much useful GPS information in the client though.
 func main() {
-	opts := append(chromedp.DefaultExecAllocatorOptions[:],
-		chromedp.UserAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.106 Safari/537.36"),
-	)
-	ctx, cancel := chromedp.NewExecAllocator(context.Background(), opts...)
-	defer cancel()
-	ctx, cancel = chromedp.NewContext(ctx)
-	defer cancel()
-
-	var res string
-	var okay bool
-	err := chromedp.Run(ctx,
-		chromedp.Navigate("https://www.vesselfinder.com/vessels/details/1012610"),
-		chromedp.AttributeValue("#djson", "data-json", &res, &okay, chromedp.NodeVisible),
-	)
-	if okay == false {
-		log.Fatal("Unable to read attribute data")
-	}
+	err := godotenv.Load()
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal("Error loading .env file")
 	}
 
-	log.Println(res)
+	url := "wss://stream.aisstream.io/v0/stream"
+	aisKey := os.Getenv("AIS_STREAM_KEY")
+	MMSI := os.Getenv("MMSI")
+
+	ws, _, err := websocket.DefaultDialer.Dial(url, nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer ws.Close()
+
+	subMsg := aisstream.SubscriptionMessage{
+		APIKey:          aisKey,
+		BoundingBoxes:   [][][]float64{{{-90.0, -180.0}, {90.0, 180.0}}}, // bounding box for the entire world
+		FiltersShipMMSI: []string{MMSI},
+	}
+
+	subMsgBytes, _ := json.Marshal(subMsg)
+	if err := ws.WriteMessage(websocket.TextMessage, subMsgBytes); err != nil {
+		log.Fatalln(err)
+	}
+
+	fmt.Println("All systems ready! Listening for new messages...")
+
+	for {
+		_, p, err := ws.ReadMessage()
+		if err != nil {
+			log.Fatalln(err)
+		}
+		var packet aisstream.AisStreamMessage
+
+		err = json.Unmarshal(p, &packet)
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		switch packet.MessageType {
+		// A vessels current position. The message we'll primarily use for saving the GPS coordinates.
+		// https://aisstream.io/documentation#PositionReport
+		case aisstream.POSITION_REPORT:
+			var positionReport aisstream.PositionReport
+			positionReport = *packet.Message.PositionReport
+			fmt.Printf("%s - MMSI: %d Latitude: %f Longitude: %f\n",
+				time.Now().Format(time.RFC822), positionReport.UserID, positionReport.Latitude, positionReport.Longitude)
+		// Any other incoming message, just log the message type for now, to get a sense of how often they come.
+		// https://aisstream.io/documentation#API-Message-Models
+		default:
+			fmt.Printf("%s - %s",
+				time.Now().Format(time.RFC822), packet.MessageType)
+		}
+	}
 }

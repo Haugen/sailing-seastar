3c0f3fed83096657002c9a479c318cd167e34f96
Haugen <tobiashaugen@gmail.com>
2024-07-30 17:29:34 +0200

    saving position report to db

--- main.go
+++ main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 	"os"
 	"time"
 
 	aisstream "github.com/aisstream/ais-message-models/golang/aisStream"
 	"github.com/gorilla/websocket"
 	"github.com/joho/godotenv"
+	"github.com/supabase-community/supabase-go"
 )
 
 func connect() (*websocket.Conn, error) {
@@ -54,6 +56,14 @@ func main() {
 		fmt.Println("Unable to load .env file")
 	}
 
+	API_URL := os.Getenv("SUPABASE_API_URL")
+	API_KEY := os.Getenv("SUPABASE_API_KEY")
+
+	db, err := supabase.NewClient(API_URL, API_KEY, nil)
+	if err != nil {
+		log.Fatal("cannot initalize Supabase client", err)
+	}
+
 	for {
 		ws := reconnect()
 		done := make(chan struct{})
@@ -70,15 +80,36 @@ func main() {
 				json.Unmarshal(p, &packet)
 
 				switch packet.MessageType {
-				// A vessels current position. The message we'll primarily use for saving the GPS coordinates.
-				// https://aisstream.io/documentation#PositionReport
 				case aisstream.POSITION_REPORT:
 					var positionReport aisstream.PositionReport
 					positionReport = *packet.Message.PositionReport
 					fmt.Printf("MMSI: %d Latitude: %f Longitude: %f\n",
 						positionReport.UserID, positionReport.Latitude, positionReport.Longitude)
-				// Any other incoming message, just log the message type for now, to get a sense of how often they come.
-				// https://aisstream.io/documentation#API-Message-Models
+
+					_, _, err := db.From("position_report").Insert(map[string]interface{}{
+						"cog":                       positionReport.Cog,
+						"communicationstate":        positionReport.CommunicationState,
+						"latitude":                  positionReport.Latitude,
+						"longitude":                 positionReport.Longitude,
+						"messageid":                 positionReport.MessageID,
+						"navigationalstatus":        positionReport.NavigationalStatus,
+						"positionaccuracy":          positionReport.PositionAccuracy,
+						"raim":                      positionReport.Raim,
+						"rateofturn":                positionReport.RateOfTurn,
+						"repeatindicator":           positionReport.RepeatIndicator,
+						"sog":                       positionReport.Sog,
+						"spare":                     positionReport.Spare,
+						"specialmanoeuvreindicator": positionReport.SpecialManoeuvreIndicator,
+						"timestamp":                 positionReport.Timestamp,
+						"trueheading":               positionReport.TrueHeading,
+						"userid":                    positionReport.UserID,
+						"valid":                     positionReport.Valid,
+					}, true, "", "", "").Execute()
+
+					if err != nil {
+						fmt.Println("Error writing to db:", err)
+					}
+
 				default:
 					fmt.Printf("%s\n",
 						packet.MessageType)

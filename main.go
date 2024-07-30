abf60d9d657c95761f86cc613e611f99aeb67bca
Haugen <tobiashaugen@gmail.com>
2024-07-30 16:04:45 +0200

    Trying another approach to reconnecting socket

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"log"
 	"os"
 	"time"
 
@@ -12,21 +11,15 @@ import (
 	"github.com/joho/godotenv"
 )
 
-func main() {
-	err := godotenv.Load()
-	if err != nil {
-		fmt.Println("Unable to load .env file")
-	}
-
-	url := "wss://stream.aisstream.io/v0/stream"
+func connect() (*websocket.Conn, error) {
 	aisKey := os.Getenv("AIS_STREAM_KEY")
 	MMSI := os.Getenv("MMSI")
+	url := "wss://stream.aisstream.io/v0/stream"
 
 	ws, _, err := websocket.DefaultDialer.Dial(url, nil)
 	if err != nil {
-		log.Fatalln(err)
+		return nil, err
 	}
-	defer ws.Close()
 
 	subMsg := aisstream.SubscriptionMessage{
 		APIKey:          aisKey,
@@ -36,36 +29,67 @@ func main() {
 
 	subMsgBytes, _ := json.Marshal(subMsg)
 	if err := ws.WriteMessage(websocket.TextMessage, subMsgBytes); err != nil {
-		log.Fatalln(err)
+		return nil, err
+	}
+	fmt.Println("Connection open.")
+
+	return ws, nil
+}
+
+func reconnect() *websocket.Conn {
+	for {
+		c, err := connect()
+		if err != nil {
+			fmt.Println("Failed to connect. Retrying...")
+			time.Sleep(60 * time.Second)
+			continue
+		}
+
+		return c
+	}
+}
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Println("Unable to load .env file")
 	}
 
-	fmt.Println("All systems ready! Listening for new messages...")
-
 	for {
-		_, p, err := ws.ReadMessage()
-		if err != nil {
-			log.Fatalln(err)
-		}
-		var packet aisstream.AisStreamMessage
+		ws := reconnect()
+		done := make(chan struct{})
 
-		err = json.Unmarshal(p, &packet)
-		if err != nil {
-			log.Fatalln(err)
-		}
+		go func() {
+			_, p, err := ws.ReadMessage()
+			if err != nil {
+				close(done)
+			}
 
-		switch packet.MessageType {
-		// A vessels current position. The message we'll primarily use for saving the GPS coordinates.
-		// https://aisstream.io/documentation#PositionReport
-		case aisstream.POSITION_REPORT:
-			var positionReport aisstream.PositionReport
-			positionReport = *packet.Message.PositionReport
-			fmt.Printf("%s - MMSI: %d Latitude: %f Longitude: %f\n",
-				time.Now().Format(time.RFC822), positionReport.UserID, positionReport.Latitude, positionReport.Longitude)
-		// Any other incoming message, just log the message type for now, to get a sense of how often they come.
-		// https://aisstream.io/documentation#API-Message-Models
-		default:
-			fmt.Printf("%s - %s\n",
-				time.Now().Format(time.RFC822), packet.MessageType)
-		}
+			var packet aisstream.AisStreamMessage
+
+			err = json.Unmarshal(p, &packet)
+			if err != nil {
+				close(done)
+			}
+
+			switch packet.MessageType {
+			// A vessels current position. The message we'll primarily use for saving the GPS coordinates.
+			// https://aisstream.io/documentation#PositionReport
+			case aisstream.POSITION_REPORT:
+				var positionReport aisstream.PositionReport
+				positionReport = *packet.Message.PositionReport
+				fmt.Printf("MMSI: %d Latitude: %f Longitude: %f\n",
+					positionReport.UserID, positionReport.Latitude, positionReport.Longitude)
+			// Any other incoming message, just log the message type for now, to get a sense of how often they come.
+			// https://aisstream.io/documentation#API-Message-Models
+			default:
+				fmt.Printf("%s\n",
+					packet.MessageType)
+			}
+		}()
+
+		<-done
+		ws.Close()
+		fmt.Println("Connection lost. Reconnecting...")
 	}
 }

02fb535597424b346928701c2d408f2c4478c10f
Haugen <tobiashaugen@gmail.com>
2024-08-01 12:05:06 +0200

    fetch locations. draw on map

--- web/src/env.d.ts
+++ web/src/env.d.ts
@@ -1,1 +1,12 @@
 /// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+  readonly SUPABASE_API_URL: string;
+  readonly SUPABASE_API_KEY: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
+
+declare const L: any;
